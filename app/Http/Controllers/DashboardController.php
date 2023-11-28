729dc1d730bbaf63ffabc6131f1d15c944a08d01
Ahmad Faisal <maulidae.rizki@gmail.com>
2023-11-28 12:17:10 +0700

    update dashboard 2

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -8,6 +8,14 @@ class DashboardController extends Controller
 {
     public function index()
     {
-        return view('dashboard.superadmin');
+        if (auth()->user()->role == "superadmin") {
+            return view('dashboard.superadmin');
+        } elseif (auth()->user()->role == "sales") {
+            return view('dashboard.sales');
+        } elseif (auth()->user()->role == "purchase") {
+            return view('dashboard.purchase');
+        } elseif (auth()->user()->role == "manager") {
+            return view('dashboard.manager');
+        }
     }
 }
